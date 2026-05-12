8d35dfc5eef10f32aadd2404ba290c2de9166acd
DGCPT Audit Platform <admin@dgcpt.ga>
2026-05-12 19:40:18 +0000

    fix(iam): add soft delete relationship support on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     protected $fillable = [
         'name',
